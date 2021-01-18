8c13153c54ed7c23e2dadd2a9c21ac922228bb37
jkli <1259491617@qq.com>
2021-01-18 23:05:34 +0800

    添加Eureka配置 将服务注册到Eureka

--- src/main/java/com/jkli/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/jkli/springbootdemo/SpringBootDemoApplication.java
@@ -2,9 +2,22 @@ package com.jkli.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+/**
+ * 主类注解打开服务注册发现客户端
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBootDemoApplication {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
--- src/main/java/com/jkli/springbootdemo/controller/HelloController.java
+++ src/main/java/com/jkli/springbootdemo/controller/HelloController.java
@@ -1,15 +1,26 @@
 package com.jkli.springbootdemo.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/controller")
 public class HelloController {
-    @Qualifier("counterService")
+//    打开日志
+    private final Logger logger = Logger.getLogger(getClass());
+//      引入服务发现客户端
+//    @Autowired
+//    private DiscoveryClient client;
+
+/*    @Qualifier("counterService")
     @Autowired
     private CounterService counterService;
 
@@ -17,10 +28,13 @@ public class HelloController {
     public String greet(){
         counterService.increment("jkli.hello.count");
         return counterService.toString();
-    }
+    }*/
 
-    @RequestMapping("/hello")
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String index(){
+//        使用log4j打印服务相关内容
+//        ServiceInstance instance = client.getLocalServiceInstance();
+//        logger.info("/hello,host:"+instance.getHost()+",service_id:"+instance.getServiceId());
         return "Hello!";
     }
 }
